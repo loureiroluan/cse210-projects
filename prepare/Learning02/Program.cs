c9f4136484b64ae566c523ea290883f8d49d92ef
loureiroluan <loureiroluan12@gmail.com>
2024-01-17 20:16:26 -0300

    updates

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Sales Manager";
+        job1._company= "Flor de Iris";
+        job1._startYear= 2019;
+        job1._endYear= 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales Agent";
+        job2._company = "USA Containers";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Luan Loureiro";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
